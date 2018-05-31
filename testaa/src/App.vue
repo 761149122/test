a36c8af5afa3c7dce72432961e8c4ba40c65f895
huangshaoping <761149122@qq.com>
2018-05-31 11:26:30 +0800

    vue-clitest

---
+++ testaa/src/App.vue
@@ -0,0 +1,29 @@
+<template>
+  <div id="app">
+  	<ul>
+  		<li>
+  			<router-link to="/">Home</router-link>
+  		</li>
+  		<li>
+  			<router-link to="/hello">Hello</router-link> <!--to组件名-->
+  		</li>
+  	</ul>
+    <router-view></router-view><!--显示home里面的html-->
+  </div>
+</template>
+
+<script>
+
+
+export default {
+  name: 'App'
+}
+</script>
+
+<style>
+h1{
+	font-size: 26px;
+	color: #42B983;
+	text-align: center;
+}
+</style>
---
+++ testaa/src/components/Home.vue
@@ -0,0 +1,45 @@
+<template>
+  <div id="home">
+    <app-head v-on:titleChanged="updateTitle($event)" ></app-head>
+    <h1>{{title}}</h1>
+    <users v-bind:users="users"></users><!--第一个users自定义 v-bind:users="users"-->
+    <users ></users>
+    <foot></foot>
+  </div>
+</template>
+
+<script>
+import Users from './Users'
+import Header from './Header'
+import Footer from './Footer'
+
+
+export default {
+  name: 'Home',
+  data(){
+  	return{
+  		title: "这是我的第一个cli脚手架项目",
+		users:["aa","bb","cc"]
+
+  	}
+  },
+  methods:{
+  	updateTitle(title){
+  		this.title = "这个是子向父组件传值";
+  	}
+  },
+  components:{
+  	"users":Users,
+  	"app-head":Header,
+  	"foot":Footer
+  }
+}
+</script>
+
+<style>
+h1{
+	font-size: 26px;
+	color: #42B983;
+	text-align: center;
+}
+</style>
